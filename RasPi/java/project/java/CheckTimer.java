c382fea24f5a1d770650a394e5207a50a813d46b
Mingyu Park <hello920922@nate.com>
2015-08-12 15:31:26 +0900

    Update CheckTimer.java

--- RasPi/java/project/java/CheckTimer.java
+++ RasPi/java/project/java/CheckTimer.java
@@ -1,7 +1,24 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class CheckTimer{
+    private Hashtable<String, Person> hashtable = null;
+    public CheckTimer(Hashtable<String, Person> hashtable){
+        this.hashtable = hashtable;
+        Thread th = new Thread(new TimerThread());
+        th.start();
+    }
+    
     class TimerThread implements Runnable{
         @Override
         public void run(){
@@ -12,11 +29,11 @@ public class CheckTimer{
                     ExecuteShell.Command_Exe("nfc-read");
                     
                     Date now = new Date();
-                    SimpleDateFormat format = new SimpleDateFormat("hh");
+                    SimpleDateFormat format = new SimpleDateFormat("H");
                     String hour = format.format(now);
-                    if(hour.equals("23")){
-                        
-                    }
+                    //if(hour.equals("23")){
+                        SendEmail();
+                    //}
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -25,9 +42,26 @@ public class CheckTimer{
     }
     public void SendEmail(){
         try{
-            
+            Set<Entry<String, Person>> set = hashtable.entrySet();
+            for(Entry<String, Person> value : set){
+                Person person = value.getValue();
+                if(person.Start != null && person.End == null){
+                    update_hash();
+                    person.End = new Date();
+                    person.Check();
+                }
+            }
+            ExecuteShell.Command_Exe("python sendMail.py");
         }catch(Exception e){
             e.printStackTrace();
         }
+    }
+    
+    public void update_hash(){
+        try{
+            ObjectOutputStream objout  = new ObjectOutputStream(new FileOutputStream("HashPhone.dat"));
+            objout.writeObject(hashtable);
+            objout.close();
+        }catch(Exception e){e.printStackTrace();}
     }
 }
