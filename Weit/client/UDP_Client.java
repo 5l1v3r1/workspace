6012ec7d199ae84fc1a9df75ebc097375ce17a88
Mingyu Park <hello920922@nate.com>
2015-08-18 10:57:38 +0900

    modify UDP_Client(add mac address)

--- Weit/client/UDP_Client.java
+++ Weit/client/UDP_Client.java
@@ -6,12 +6,17 @@ public class UDP_Client{
         String bcast = getBcast();
         String serverip = UDPsend(bcast);
         String macaddr = getMACAddr();
+        System.out.println(macaddr);
         startNFC(serverip, macaddr);
     }
     
     public static void startNFC(String ip, String macaddr) throws Exception{
         Runtime runtime = Runtime.getRuntime();
-        runtime.exec("nfc-read " + ip + " " + macaddr);
+        ip = ip.trim();
+        macaddr = macaddr.trim();
+        String command = "nfc-read " + ip + " " + macaddr;
+        System.out.println(command);
+        runtime.exec(command);
     }
 
     public static String UDPsend(String bcast) throws Exception{
@@ -35,14 +40,37 @@ public class UDP_Client{
     }
 
     public static String getMACAddr() throws Exception{
-        String command = "ifconfig -a | grep &wlan | awk '{print $5}'";
+        String command = "ifconfig";
         Runtime runtime = Runtime.getRuntime();
         Process process = runtime.exec(command);
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line = reader.readLine();
 
-        return line;
+        String line="";
+        String l;
+
+        while((l = reader.readLine())!=null)
+            line += l+"\n";
+
+        String iparr[] = line.split("\n\n");
+        String MACAddr = null;
+        for(String ip : iparr){
+            if(ip.contains("wlan")){
+                ip = ip.replace("\n","");
+                ip = ip.replace("\t","");
+                ip = ip.replace(" ","");
+                int start_index = ip.indexOf("HWaddr") + 6;
+                int end_index = ip.indexOf("inetaddr");
+                MACAddr = ip.substring(start_index, end_index);
+            }
+        }
+        if(MACAddr == null){
+            System.out.println("Not Found wlan");
+            System.exit(0);
+        }
+        return MACAddr;
     }
+
+
     public static String getBcast() throws Exception{
         String command = "ifconfig";
         Runtime runtime = Runtime.getRuntime();
