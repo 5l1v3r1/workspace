80e7a8909b5426a34e8b9913d50f10006d94d18b
Mingyu Park <hello920922@nate.com>
2015-08-21 11:16:51 +0900

    add function(print total worktime)

--- Weit/page/instructor/function_manager.php
+++ Weit/page/instructor/function_manager.php
@@ -47,12 +47,68 @@ function printEmail(){
     else
         echo $row[4];
 }
-function printLog($yy, $mm){
+function printTotal($uid, $yy, $mm){
     global $con;
-    $querycom = "SELECT DAY, CHECKIN, CHECKOUT, WORKTIME";
-    $querycom = $querycom." WHERE YEAR(DAY)=".$yy." AND MONTH(DAY)=".$mm;
-    if($result=$con->query($querycom)){
-        while
+    $querycom = "SELECT SUM(WORKTIME) FROM INSTLOG";
+    $querycom = $querycom." WHERE UID='".$uid."'";
+    $querycom = $querycom." AND YEAR(DAY)=".$yy." AND MONTH(DAY)=".$mm;
+    if($result = $con->query($querycom)){
+        if($rows = $result->fetch_array(MYSQL_NUM)){
+            echo $rows[0];
+        }
+    }
+}
+function printLog($uid, $yy, $mm){
+    global $con;
+    $querycom = "SELECT DAY, CHECKIN, CHECKOUT, WORKTIME FROM INSTLOG";
+    $querycom = $querycom." WHERE UID='".$uid."'";
+    $querycom = $querycom." AND YEAR(DAY)=".$yy." AND MONTH(DAY)=".$mm;
+    if($result = $con->query($querycom)){
+        while($rows = $result->fetch_array(MYSQL_NUM)){
+            echo "  ";
+            echo "<tr>\n\r";
+            echo "      ";
+            echo "<td align=\"center\" width=\"30%\">\n\r";
+            echo "          ";
+            echo $rows[0]."\n\r";
+            echo "      ";
+            echo "</td>\n\r";
+            echo "  ";
+            echo "</tr>\n\r";
+
+            echo "  ";
+            echo "<tr>\n\r";
+            echo "      ";
+            echo "<td align=\"center\" width=\"30%\">\n\r";
+            echo "          ";
+            echo $rows[1]."\n\r";
+            echo "      ";
+            echo "</td>\n\r";
+            echo "  ";
+            echo "</tr>\n\r";
+
+            echo "  ";
+            echo "<tr>\n\r";
+            echo "      ";
+            echo "<td align=\"center\" width=\"30%\">\n\r";
+            echo "          ";
+            echo $rows[2]."\n\r";
+            echo "      ";
+            echo "</td>\n\r";
+            echo "  ";
+            echo "</tr>\n\r";
+
+            echo "  ";
+            echo "<tr>\n\r";
+            echo "      ";
+            echo "<td align=\"center\" width=\"10%\">\n\r";
+            echo "          ";
+            echo $rows[3]."\n\r";
+            echo "      ";
+            echo "</td>\n\r";
+            echo "  ";
+            echo "</tr>\n\r";
+        }
     }
 }
 ?>
